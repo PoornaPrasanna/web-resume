969bfa9c6d112ade0183fc27f40ac0f5528df78a
PoornaPrasanna <96341110+PoornaPrasanna@users.noreply.github.com>
2024-11-20 10:42:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const btn = document.getElementById('openCloseBtn');
+    const sidebar = document.querySelector('.sidebar');
+
+    btn.addEventListener('click', function() {
+        sidebar.classList.toggle('active');
+    });
+
+})
